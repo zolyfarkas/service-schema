45565ded5c18abcd4da1499daffaf9ef3355192f
Zoltan Farkas <zolyfarkas@yahoo.com>
2017-05-26 14:34:03 -0400

    [AVRO-1723] sync this up with AVRO implementation.

--- lang/java/compiler/src/main/java/org/apache/avro/compiler/idl/SchemaResolver.java
+++ lang/java/compiler/src/main/java/org/apache/avro/compiler/idl/SchemaResolver.java
@@ -15,203 +15,132 @@
  */
 package org.apache.avro.compiler.idl;
 
+import com.google.common.base.Function;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.avro.Protocol;
 import org.apache.avro.Schema;
+import org.apache.avro.compiler.schema.Schemas;
 
 /**
- *
- * @author zoly
+ * Utility class to resolve schemas that are unavailable at the time they are referenced in the IDL.
  */
 final class SchemaResolver {
 
-    private SchemaResolver() { }
+  private SchemaResolver() {
+  }
 
-    private static final String UR_SCHEMA_ATTR = "org.apache.avro.compiler.idl.unresolved.name";
+  private static final String UR_SCHEMA_ATTR = "org.apache.avro.compiler.idl.unresolved.name";
 
-    static Schema unresolvedSchema(final String name) {
-        Schema schema = Schema.createRecord("UnresolvedSchema", "unresolved schema",
-                "org.apache.avro.compiler", false, Collections.EMPTY_LIST);
-        schema.addProp(UR_SCHEMA_ATTR, name);
-        return schema;
+  private static final String UR_SCHEMA_NAME = "UnresolvedSchema";
+
+  private static final String UR_SCHEMA_NS = "org.apache.avro.compiler";
+
+  /**
+   * Create a schema to represent a "unresolved" schema.
+   * (used to represent a schema where the definition is not known at the time)
+   * This concept might be generalizable...
+   *
+   * @param name
+   * @return
+   */
+  static Schema unresolvedSchema(final String name) {
+    Schema schema = Schema.createRecord(UR_SCHEMA_NAME, "unresolved schema",
+        UR_SCHEMA_NS, false, Collections.EMPTY_LIST);
+    schema.addProp(UR_SCHEMA_ATTR, name);
+    return schema;
+  }
+
+  /**
+   * Is this a unresolved schema.
+   *
+   * @param schema
+   * @return
+   */
+  static boolean isUnresolvedSchema(final Schema schema) {
+    return (schema.getType() == Schema.Type.RECORD && schema.getProp(UR_SCHEMA_ATTR) != null
+        && UR_SCHEMA_NAME.equals(schema.getName())
+        && UR_SCHEMA_NS.equals(schema.getNamespace()));
+  }
+
+  /**
+   * get the unresolved schema name.
+   *
+   * @param schema
+   * @return
+   */
+  static String getUnresolvedSchemaName(final Schema schema) {
+    if (!isUnresolvedSchema(schema)) {
+      throw new IllegalArgumentException("Not a unresolved schema: " + schema);
+    }
+    return schema.getProp(UR_SCHEMA_ATTR);
+  }
+
+  /**
+   * Will clone the provided protocol while resolving all unreferenced schemas
+   *
+   * @param protocol
+   * @return
+   */
+  static Protocol resolve(final Protocol protocol) {
+    Protocol result = new Protocol(protocol.getName(), protocol.getDoc(), protocol.getNamespace());
+    final Collection<Schema> types = protocol.getTypes();
+    // replace unresolved schemas.
+    List<Schema> newSchemas = new ArrayList(types.size());
+    IdentityHashMap<Schema, Schema> replacements = new IdentityHashMap<Schema, Schema>();
+    for (Schema schema : types) {
+      newSchemas.add(Schemas.visit(schema, new ResolvingVisitor(schema, replacements, new SymbolTable(protocol))));
+    }
+    result.setTypes(newSchemas); // replace types with resolved ones
+
+    // Resolve all schemas refferenced by protocol Messages.
+    for (Map.Entry<String, Protocol.Message> entry : protocol.getMessages().entrySet()) {
+      Protocol.Message value = entry.getValue();
+      Protocol.Message nvalue;
+      if (value.isOneWay()) {
+        Schema replacement = resolve(replacements, value.getRequest(), protocol);
+        nvalue = result.createMessage(value.getName(), value.getDoc(),
+            value.getObjectProps(), replacement);
+      } else {
+        Schema request = resolve(replacements, value.getRequest(), protocol);
+        Schema response = resolve(replacements, value.getResponse(), protocol);
+        Schema errors = resolve(replacements, value.getErrors(), protocol);
+        nvalue = result.createMessage(value.getName(), value.getDoc(),
+            value.getObjectProps(), request, response, errors);
+      }
+      result.getMessages().put(entry.getKey(), nvalue);
+    }
+    Schemas.copyProperties(protocol, result);
+    return result;
+  }
+
+  private static Schema resolve(final IdentityHashMap<Schema, Schema> replacements,
+                                final Schema request, final Protocol protocol) {
+    Schema replacement = replacements.get(request);
+    if (replacement == null) {
+      replacement = Schemas.visit(request, new ResolvingVisitor(request, replacements,
+          new SymbolTable(protocol)));
+    }
+    return replacement;
+  }
+
+  private static class SymbolTable implements Function<String, Schema> {
+
+    private final Protocol symbolTable;
+
+    public SymbolTable(Protocol symbolTable) {
+      this.symbolTable = symbolTable;
     }
 
-    static boolean isUnresolvedSchema(final Schema schema) {
-        return (schema.getType() == Schema.Type.RECORD && schema.getProp(UR_SCHEMA_ATTR) != null);
+    @Override
+    public Schema apply(final String f) {
+      return symbolTable.getType(f);
     }
-
-    static String getUnresolvedSchemaName(final Schema schema) {
-        String name = schema.getProp(UR_SCHEMA_ATTR);
-        if (name == null) {
-            throw new IllegalArgumentException("Schema " + schema + " is not a unresolved schema");
-        } else {
-            return name;
-        }
-    }
-
-
-
-    static Protocol resolve(final Protocol protocol) {
-        Protocol result = new Protocol(protocol.getName(), protocol.getDoc(), protocol.getNamespace());
-        final Collection<Schema> types = protocol.getTypes();
-        List<Schema> newSchemas = new ArrayList(types.size());
-        Map<String, Schema> processed =  new HashMap<String, Schema>();
-        for (Schema schema : types) {
-            newSchemas.add(resolve(schema, protocol, processed));
-        }
-        result.setTypes(newSchemas);
-
-        for (Map.Entry<String, Protocol.Message> entry : protocol.getMessages().entrySet()) {
-            Protocol.Message value = entry.getValue();
-            Protocol.Message nvalue;
-            if (value.isOneWay()) {
-                Schema request = value.getRequest();
-                nvalue = result.createMessage(value.getName(), value.getDoc(),
-                        value.getObjectProps(), intern(request, processed));
-            } else {
-                Schema request = value.getRequest();
-                Schema response = value.getResponse();
-                Schema errors = value.getErrors();
-                nvalue = result.createMessage(value.getName(), value.getDoc(),
-                        value.getObjectProps(), intern(request, processed),
-                        intern(response, processed), intern(errors, processed));
-            }
-            result.getMessages().put(entry.getKey(), nvalue);
-        }
-        result.addJsonProps(protocol.getJsonProps());
-        return result;
-    }
-
-    static Schema resolve(final Schema schema, final Protocol protocol, final Map<String, Schema> processed) {
-        final String fullName = schema.getFullName();
-        if (fullName != null && processed.containsKey(fullName)) {
-            return processed.get(schema.getFullName());
-        } else if (isUnresolvedSchema(schema)) {
-            final String unresolvedSchemaName = getUnresolvedSchemaName(schema);
-            Schema type = protocol.getType(unresolvedSchemaName);
-            if (type == null) {
-                throw new IllegalArgumentException("Cannot resolve " + unresolvedSchemaName);
-            }
-            return resolve(type, protocol, processed);
-        } else {
-            switch (schema.getType()) {
-                case RECORD:
-                    Schema createRecord = Schema.createRecord(schema.getName(), schema.getDoc(), schema.getNamespace(),
-                    schema.isError());
-                    processed.put(schema.getFullName(), createRecord);
-                    final List<Schema.Field> currFields = schema.getFields();
-                    List<Schema.Field> newFields = new ArrayList<Schema.Field>(currFields.size());
-                    for (Schema.Field field : currFields) {
-                        Schema.Field nf = new Schema.Field(field.name(), resolve(field.schema(), protocol, processed),
-                                field.doc(), field.defaultVal(), field.order());
-                        for (String alias : field.aliases()) {
-                            nf.addAlias(alias);
-                        }
-                        newFields.add(nf);
-                    }
-                    createRecord.setFields(newFields);
-                    if (schema.getLogicalType() != null) {
-                        createRecord.setLogicalType(schema.getLogicalType());
-                    }
-                    createRecord.addJsonProps(schema.getJsonProps());
-                    return createRecord;
-                case MAP:
-                    Schema result = Schema.createMap(resolve(schema.getValueType(), protocol, processed));
-                    result.addJsonProps(schema.getJsonProps());
-                    return result;
-                case ARRAY:
-                    Schema aresult = Schema.createArray(resolve(schema.getElementType(), protocol, processed));
-                    aresult.addJsonProps(schema.getJsonProps());
-                    return aresult;
-                case UNION:
-                    final List<Schema> uTypes = schema.getTypes();
-                    List<Schema> newTypes = new ArrayList<Schema>(uTypes.size());
-                    for (Schema s : uTypes) {
-                        newTypes.add(resolve(s, protocol, processed));
-                    }
-                    Schema bresult =  Schema.createUnion(newTypes);
-                    bresult.addJsonProps(schema.getJsonProps());
-                    return bresult;
-                case ENUM:
-                case FIXED:
-                case STRING:
-                case BYTES:
-                case INT:
-                case LONG:
-                case FLOAT:
-                case DOUBLE:
-                case BOOLEAN:
-                case NULL:
-                    return schema;
-                default:
-                    throw new RuntimeException("Unknown type: " + schema);
-            }
-        }
-    }
-
-    public static Schema intern(final Schema schema, final Map<String, Schema> processed) {
-        if (schema == null) {
-            return null;
-        }
-        final String fullName = schema.getFullName();
-        if (fullName != null && processed.containsKey(fullName)) {
-            return processed.get(schema.getFullName());
-        } else {
-            switch (schema.getType()) {
-                case RECORD:
-                    Schema createRecord = Schema.createRecord(schema.getName(), schema.getDoc(), schema.getNamespace(),
-                    schema.isError());
-                    processed.put(schema.getFullName(), createRecord);
-                    final List<Schema.Field> currFields = schema.getFields();
-                    List<Schema.Field> newFields = new ArrayList<Schema.Field>(currFields.size());
-                    for (Schema.Field field : currFields) {
-                        Schema.Field nf = new Schema.Field(field.name(), intern(field.schema(), processed),
-                                field.doc(), field.defaultVal(), field.order());
-                        for (String alias : field.aliases()) {
-                            nf.addAlias(alias);
-                        }
-                        newFields.add(nf);
-                    }
-                    createRecord.setFields(newFields);
-                    if (schema.getLogicalType() != null) {
-                        createRecord.setLogicalType(schema.getLogicalType());
-                    }
-                    createRecord.addJsonProps(schema.getJsonProps());
-                    return createRecord;
-                case MAP:
-                    return Schema.createMap(intern(schema.getValueType(), processed));
-                case ARRAY:
-                    return Schema.createArray(intern(schema.getElementType(), processed));
-                case UNION:
-                    final List<Schema> uTypes = schema.getTypes();
-                    List<Schema> newTypes = new ArrayList<Schema>(uTypes.size());
-                    for (Schema s : uTypes) {
-                        newTypes.add(intern(s, processed));
-                    }
-                    return Schema.createUnion(newTypes);
-                case ENUM:
-                case FIXED:
-                case STRING:
-                case BYTES:
-                case INT:
-                case LONG:
-                case FLOAT:
-                case DOUBLE:
-                case BOOLEAN:
-                case NULL:
-                    return schema;
-                default:
-                    throw new RuntimeException("Unknown type: " + schema);
-            }
-        }
-    }
-
-
-
+  }
 
 }
--- lang/java/compiler/src/test/java/org/apache/avro/compiler/idl/TestCycle.java
+++ lang/java/compiler/src/test/java/org/apache/avro/compiler/idl/TestCycle.java
@@ -23,11 +23,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.apache.avro.Protocol;
 import org.apache.avro.Schema;
 import org.apache.avro.compiler.specific.SpecificCompiler;
-import org.apache.avro.generic.ExtendedGenericDatumWriter;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
@@ -35,89 +34,70 @@ import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.avro.io.BinaryDecoder;
 import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DecoderFactory;
-import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
-import org.apache.avro.io.ExtendedJsonDecoder;
-import org.apache.avro.io.ExtendedJsonEncoder;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- *
- * @author zoly
- */
 public class TestCycle {
 
-    @Test
-    public void testCycleGeneration() throws ParseException, IOException {
-        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        Idl idl = new Idl(cl.getResourceAsStream("cycle/cycle.avdl"),
-                "UTF-8");
-        Protocol protocol = idl.CompilationUnit();
-        String json = protocol.toString();
-        System.out.println(json);
+  private static final Logger LOG = LoggerFactory.getLogger(TestCycle.class);
 
-        SpecificCompiler compiler = new SpecificCompiler(protocol);
-        compiler.setStringType(GenericData.StringType.String);
-        File output = new File("./target");
-        SpecificCompiler.SCHEMA_OUTPUT_DIR.set(output);
-        compiler.compileToDestination(null, output);
+  @Test
+  public void testCycleGeneration() throws ParseException, IOException {
+    final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    Idl idl = new Idl(cl.getResourceAsStream("input/cycle.avdl"),
+            "UTF-8");
+    Protocol protocol = idl.CompilationUnit();
+    String json = protocol.toString();
+    LOG.info(json);
 
-        Map<String, Schema> schemas = new HashMap<String, Schema>();
-        for (Schema schema : protocol.getTypes()) {
-            final String name = schema.getName();
-            schemas.put(name, schema);
-        }
+    SpecificCompiler compiler = new SpecificCompiler(protocol);
+    compiler.setStringType(GenericData.StringType.String);
+    File output = new File("./target");
+    compiler.compileToDestination(null, output);
 
-        GenericRecordBuilder rb2 = new GenericRecordBuilder(schemas.get("SampleNode"));
-        rb2.set("count", 10);
-        rb2.set("subNodes", Collections.EMPTY_LIST);
-        GenericData.Record node = rb2.build();
-
-        GenericRecordBuilder mb = new GenericRecordBuilder(schemas.get("Method"));
-        mb.set("declaringClass", "Test");
-        mb.set("methodName", "test");
-        GenericData.Record method = mb.build();
-
-        GenericRecordBuilder spb = new GenericRecordBuilder(schemas.get("SamplePair"));
-        spb.set("method", method);
-        spb.set("node", node);
-        GenericData.Record sp = spb.build();
-
-
-        GenericRecordBuilder rb = new GenericRecordBuilder(schemas.get("SampleNode"));
-        rb.set("count", 10);
-        rb.set("subNodes", Arrays.asList(sp));
-        GenericData.Record record = rb.build();
-
-        serDeserRecord(record);
-        serDeserRecordJson(record);
-
+    Map<String, Schema> schemas = new HashMap<String, Schema>();
+    for (Schema schema : protocol.getTypes()) {
+      final String name = schema.getName();
+      schemas.put(name, schema);
     }
 
-    private static void serDeserRecord(GenericData.Record data) throws IOException {
-        ByteArrayOutputStream bab = new ByteArrayOutputStream();
-        GenericDatumWriter writer = new GenericDatumWriter(data.getSchema());
-        final BinaryEncoder directBinaryEncoder = EncoderFactory.get().directBinaryEncoder(bab, null);
-        writer.write(data, directBinaryEncoder);
-        directBinaryEncoder.flush();
-        ByteArrayInputStream bis = new ByteArrayInputStream(bab.toByteArray(), 0, bab.size());
-        GenericDatumReader reader = new GenericDatumReader(data.getSchema());
-        BinaryDecoder directBinaryDecoder = DecoderFactory.get().directBinaryDecoder(bis, null);
-        GenericData.Record read = (GenericData.Record) reader.read(null, directBinaryDecoder);
-        Assert.assertEquals(data.toString(), read.toString());
-    }
+    GenericRecordBuilder rb2 = new GenericRecordBuilder(schemas.get("SampleNode"));
+    rb2.set("count", 10);
+    rb2.set("subNodes", Collections.EMPTY_LIST);
+    GenericData.Record node = rb2.build();
 
-    private static void serDeserRecordJson(GenericData.Record data) throws IOException {
-        ByteArrayOutputStream bab = new ByteArrayOutputStream();
-        GenericDatumWriter writer = new ExtendedGenericDatumWriter(data.getSchema());
-        final Encoder encoder = new ExtendedJsonEncoder(data.getSchema(), bab);
-        writer.write(data, encoder);
-        encoder.flush();
-        ByteArrayInputStream bis = new ByteArrayInputStream(bab.toByteArray(), 0, bab.size());
-        GenericDatumReader reader = new GenericDatumReader(data.getSchema());
-        ExtendedJsonDecoder directBinaryDecoder = new ExtendedJsonDecoder(data.getSchema(), bis);
-        GenericData.Record read = (GenericData.Record) reader.read(null, directBinaryDecoder);
-        Assert.assertEquals(data.toString(), read.toString());
-    }
+    GenericRecordBuilder mb = new GenericRecordBuilder(schemas.get("Method"));
+    mb.set("declaringClass", "Test");
+    mb.set("methodName", "test");
+    GenericData.Record method = mb.build();
+
+    GenericRecordBuilder spb = new GenericRecordBuilder(schemas.get("SamplePair"));
+    spb.set("method", method);
+    spb.set("node", node);
+    GenericData.Record sp = spb.build();
+
+    GenericRecordBuilder rb = new GenericRecordBuilder(schemas.get("SampleNode"));
+    rb.set("count", 10);
+    rb.set("subNodes", Arrays.asList(sp));
+    GenericData.Record record = rb.build();
+
+    serDeserRecord(record);
+
+  }
+
+  private static void serDeserRecord(GenericData.Record data) throws IOException {
+    ByteArrayOutputStream bab = new ByteArrayOutputStream();
+    GenericDatumWriter writer = new GenericDatumWriter(data.getSchema());
+    final BinaryEncoder directBinaryEncoder = EncoderFactory.get().directBinaryEncoder(bab, null);
+    writer.write(data, directBinaryEncoder);
+    directBinaryEncoder.flush();
+    ByteArrayInputStream bis = new ByteArrayInputStream(bab.toByteArray(), 0, bab.size());
+    GenericDatumReader reader = new GenericDatumReader(data.getSchema());
+    BinaryDecoder directBinaryDecoder = DecoderFactory.get().directBinaryDecoder(bis, null);
+    GenericData.Record read = (GenericData.Record) reader.read(null, directBinaryDecoder);
+    Assert.assertEquals(data.toString(), read.toString());
+  }
 
 }
