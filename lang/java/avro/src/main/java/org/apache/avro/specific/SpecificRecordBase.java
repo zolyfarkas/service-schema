fff10d9930bda0732c3ced2e007e84e20ea6e045
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-01-10 10:14:55 -0500

    [fix] potential npe.

--- lang/java/avro/src/main/java/org/apache/avro/specific/SpecificRecordBase.java
+++ lang/java/avro/src/main/java/org/apache/avro/specific/SpecificRecordBase.java
@@ -47,6 +47,7 @@ public abstract class SpecificRecordBase
   @Override
   public boolean equals(Object that) {
     if (that == this) return true;                        // identical object
+    if (that == null) return false;
     if (this.getClass() != that.getClass()) return false; // not same schema
     return SpecificData.get().compare(this, that, this.getSchema(), true) == 0;
   }
